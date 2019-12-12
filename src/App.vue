6e9b490e8d8109f3e1891695a0f3b2bd3caac8f1
陈文华 <wendell.chen@chinaentropy.com>
2019-12-12 15:43:06 +0800

    feat webpack4.0 + vue 项目基础

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<!--
+	/**
+		 * @author: wen_dell
+		 * @createDate: 2019/12/10
+		 * @weChat: fourteen_clever
+		 * **/
+-->
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data(){
+            return {
+                template: {
+                    inputParam: []
+                }
+            }
+        },
+        methods: {
+            test(){
+                this.template.inputParam({
+                    xxx: 'xxx',
+                    'dsfaf': 'ssdf'
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
